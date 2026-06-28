2ea04b7bf1d04ea42e34186aad2a547dc40ba2b4
Hugo Bekkenk <s1141576@student.hsleiden.nl>
2026-06-28 17:15:42 +0200

    feature: made creature to a group of creatures

--- rust/src/bridge.rs
+++ rust/src/bridge.rs
@@ -1,20 +1,25 @@
-use godot::prelude::*;
-use crate::game::creature::{Creature, Direction, MovementState};
+use crate::game::creature::{BehaviorState, Creature, Direction, MovementState};
 use crate::game::vec2::Vec2;
+use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node)]
 struct HearthBridge {
     base: Base<Node>,
-    creature: Creature
+    pub creatures: Vec<Creature>,
+    pub selected_creatures: Vec<u32>,
+    pub next_id: u32,
 }
 
+// Godot functions
 #[godot_api]
 impl INode for HearthBridge {
     fn init(base: Base<Node>) -> Self {
         Self {
             base,
-            creature: Creature::new()
+            creatures: vec![],
+            selected_creatures: vec![],
+            next_id: 0,
         }
     }
 
@@ -23,36 +28,88 @@ impl INode for HearthBridge {
     }
 
     fn process(&mut self, delta: f64) {
-        if !self.creature.is_at_target() {
-            self.creature.move_towards_target(delta as f32)
-        } else {
-            self.creature.movement_state = MovementState::Idle;
+        for creature in self.creatures.iter_mut() {
+            if !creature.is_at_target() {
+                creature.move_towards_target(delta as f32)
+            } else if creature.behavior_state == BehaviorState::Wandering {
+                creature.wander(delta as f32)
+            } else {
+                creature.movement_state = MovementState::Idle;
+            }
         }
     }
 }
 
+// Public functions
 #[godot_api]
-impl  HearthBridge {
+impl HearthBridge {
     #[func]
-    pub fn get_creature_position(&mut self) -> Vector2 {
-        Vector2 {x: self.creature.position.x, y: self.creature.position.y}
+    pub fn spawn_creature(&mut self) -> u32 {
+        let current_id = self.next_id;
+        let new_creature = Creature::new(self.next_id);
+        self.creatures.push(new_creature);
+        self.next_id += 1;
+        current_id
+    }
+
+    #[func]
+    pub fn get_creature_position(&mut self, id: u32) -> Vector2 {
+        if let Some(creature) = self.find_creature(id) {
+            Vector2 {
+                x: creature.position.x,
+                y: creature.position.y,
+            }
+        } else {
+            godot_error!("Creature with id {} not found", id);
+            Vector2::ZERO
+        }
+    }
+
+    #[func]
+    pub fn select_creature(&mut self, id: u32) {
+        if self.selected_creatures.contains(&id) {
+            self.selected_creatures.retain(|&c| c != id);
+        } else {
+            self.selected_creatures.push(id);
+        }
     }
 
     #[func]
     pub fn set_creature_target(&mut self, target: Vector2) {
-        self.creature.target = Vec2 { x: target.x, y: target.y };
+        let ids: Vec<u32> = self.selected_creatures.clone();
+        for creature_id in ids {
+            if let Some(creature) = self.find_creature(creature_id) {
+                creature.target = Vec2 {
+                    x: target.x,
+                    y: target.y,
+                };
+                creature.behavior_state = BehaviorState::BeingOrdered;
+            }
+        }
     }
 
     #[func]
-    pub fn get_animation_name(&self) -> GString {
-        match &self.creature.movement_state {
-            MovementState::Idle => "idle".into(),
-            MovementState::Moving(dir) => match dir {
-                Direction::Up => "moving_up".into(),
-                Direction::Down => "moving_down".into(),
-                Direction::Left => "moving_left".into(),
-                Direction::Right => "moving_right".into(),
+    pub fn get_animation_name(&mut self, id: u32) -> GString {
+        if let Some(creature) = self.find_creature(id) {
+            match &creature.movement_state {
+                MovementState::Idle => "idle".into(),
+                MovementState::Moving(dir) => match dir {
+                    Direction::Up => "moving_up".into(),
+                    Direction::Down => "moving_down".into(),
+                    Direction::Left => "moving_left".into(),
+                    Direction::Right => "moving_right".into(),
+                },
             }
+        } else {
+            godot_error!("Creature with id {} not found", id);
+            "idle".into()
         }
+    }
+}
+
+// private functions
+impl HearthBridge {
+    fn find_creature(&mut self, id: u32) -> Option<&mut Creature> {
+        self.creatures.iter_mut().find(|c| c.id == id)
     }
 }
--- rust/src/game/vec2.rs
+++ rust/src/game/vec2.rs
@@ -1,27 +1,48 @@
 pub struct Vec2 {
     pub x: f32,
-    pub y: f32
+    pub y: f32,
 }
 
 impl Vec2 {
-    pub fn length (&self) -> f32 {
+    pub fn length(&self) -> f32 {
         (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
     }
 
     pub fn normalize(&self) -> Vec2 {
         let len = self.length();
-        Vec2 { x: self.x / len, y: self.y / len }
+        Vec2 {
+            x: self.x / len,
+            y: self.y / len,
+        }
     }
 
     pub fn subtract(&self, other: &Vec2) -> Vec2 {
-        Vec2 { x: self.x - other.x, y: self.y - other.y }
+        Vec2 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
     }
 
     pub fn add(&self, other: &Vec2) -> Vec2 {
-        Vec2 { x: self.x + other.x, y: self.y + other.y }
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
     }
 
     pub fn scale(&self, factor: f32) -> Vec2 {
-        Vec2 { x: self.x * factor, y: self.y * factor }
+        Vec2 {
+            x: self.x * factor,
+            y: self.y * factor,
+        }
+    }
+
+    pub fn point_in_direction(&self, angle: f64, distance: f64) -> Vec2 {
+        let x_point = angle.cos() * distance;
+        let y_point = angle.sin() * distance;
+        self.add(&Vec2 {
+            x: x_point as f32,
+            y: y_point as f32,
+        })
     }
 }
