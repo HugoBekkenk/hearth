cd58db2ffb436b077d47fab531d07fc256b7855e
Hugo Bekkenk <s1141576@student.hsleiden.nl>
2026-07-02 17:26:21 +0200

    feature: made sure the world is generated as an island

--- rust/src/game/world_generation.rs
+++ rust/src/game/world_generation.rs
@@ -2,11 +2,11 @@ use crate::game::grid_pos::GridPos;
 use crate::game::terrain_type::TerrainType;
 use crate::game::tile::Tile;
 use crate::game::tile_content::TileContent;
-use noise::{NoiseFn, Perlin};
+use noise::{NoiseFn, OpenSimplex};
 use std::collections::HashMap;
 
 pub fn generate(width: i32, height: i32, seed: u32) -> HashMap<GridPos, Tile> {
-    let perlin = Perlin::new(seed);
+    let simplex = OpenSimplex::new(seed);
     let mut map: HashMap<GridPos, Tile> = HashMap::new();
 
     for x in 0..width {
@@ -14,7 +14,7 @@ pub fn generate(width: i32, height: i32, seed: u32) -> HashMap<GridPos, Tile> {
             map.insert(
                 GridPos { x, y },
                 Tile {
-                    terrain: generate_terrain_type(&perlin, x, y),
+                    terrain: generate_terrain_type(&simplex, x, y, width, height),
                     content: TileContent::Empty,
                 },
             );
@@ -23,8 +23,20 @@ pub fn generate(width: i32, height: i32, seed: u32) -> HashMap<GridPos, Tile> {
     map
 }
 
-fn generate_terrain_type(perlin: &Perlin, x: i32, y: i32) -> TerrainType {
-    let height_value = perlin.get([x as f64 * 0.1, y as f64 * 0.1]);
+fn generate_terrain_type(
+    simplex: &OpenSimplex,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+) -> TerrainType {
+    let x_offset = (x - width / 2) as f64;
+    let y_offset = (y - height / 2) as f64;
+    let distance = (x_offset.powi(2) + y_offset.powi(2)).sqrt();
+    let max_distance = ((width as f64 / 2.0).powi(2) + (height as f64 / 2.0).powi(2)).sqrt();
+    let normalized_distance = distance / max_distance;
+    let mut height_value = simplex.get([x as f64 * 0.05, y as f64 * 0.05]);
+    height_value -= normalized_distance * 0.8;
     if height_value <= 0.0 {
         TerrainType::Water
     } else if height_value < 0.1 {
